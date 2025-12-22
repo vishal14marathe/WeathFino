98408cdb2bb6eaa081ec753a66999f97ef19aa4a
vishal14marathe <vishalmarathe@gmail.com>
2025-12-22 22:53:34 +0530

    vishal

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
 
       <UHome />
 
-      {/* <Hero />
+      {/* <Hero /> */}
 
       <MarketTicker />
 
@@ -30,7 +30,7 @@ export default function App() {
       <WhyChooseUs />
       <Stats />
       <Testimonials />
-       */}
+
       <Footer />
     </>
   );
